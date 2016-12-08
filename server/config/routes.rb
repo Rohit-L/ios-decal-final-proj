8d71f478210489a58949f34045f1a1d32b5cd24c
Rohit Lalchandani <rohitx15@berkeley.edu>
2016-12-07 20:47:50 -0800

    Added profile icon; finished with app for now

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/item/create', to: 'application#create_item'
+  post '/item/create', to: 'application#create_item'
   get '/item/read', to: 'application#read_item'
   post '/user/create', to: 'application#create_user'
   post '/item/update', to: 'application#update_item'
